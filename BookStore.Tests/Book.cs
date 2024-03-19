7b719ce304fabfe9914fab02e8cbd0d75edb94be
“Ervis <ervistrupja@outlook.com>
2024-03-19 17:11:17 +0100

    Add project files.

---
+++ BookStore.Tests/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Tests
+{
+    public class Book
+    {
+        public Book() { }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+
+            Console.WriteLine($"(Constructor) Book Id: {Id} Title: {Title}");
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ BookStore.Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Tests;
+
+Console.WriteLine("------ Variable Start -------");
+string fullName = "Emri Mbiemri";
+int age = 20;
+Console.WriteLine($"Emri:{fullName} Mosha: {age}"); //string interpolation
+
+char gender = 'M';
+DateTime dob = DateTime.Now.AddDays(-20);
+
+var firstName = "Emri";
+var lastName = 10;
+Console.WriteLine("------ Variable End -------");
+
+Console.WriteLine("------ Class Start -------");
+
+var newBook = new Book();
+newBook.Id = 1;
+newBook.Title = "Book Title";
+Console.WriteLine($"Book Id: {newBook.Id} Title: {newBook.Title}");
+
+var secondBook = new Book(2, "Second Book");
+Console.WriteLine($"Book Id: {secondBook.Id} Title: {secondBook.Title}");
+
+string stopHere = "";
