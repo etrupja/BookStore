7809a8d276f232429ee1b4595cd4f0e8412f7210
“Ervis <ervistrupja@outlook.com>
2024-03-26 17:13:21 +0100

    Shembuj listat

--- BookStore.Tests/Book.cs
+++ BookStore.Tests/Book.cs
@@ -9,15 +9,15 @@ namespace BookStore.Tests
     public class Book
     {
         public Book() { }
-        public Book(int id, string title)
+        public Book(int id, string title, int numberOfPages)
         {
             Id = id;
             Title = title;
-
-            Console.WriteLine($"(Constructor) Book Id: {Id} Title: {Title}");
+            NrOfPages = numberOfPages;
         }
 
         public int Id { get; set; }
         public string Title { get; set; }
+        public int NrOfPages { get; set; }
     }
 }
--- BookStore.Tests/Program.cs
+++ BookStore.Tests/Program.cs
@@ -1,25 +1,81 @@
-﻿using BookStore.Tests;
+﻿//using BookStore.Tests;
 
-Console.WriteLine("------ Variable Start -------");
-string fullName = "Emri Mbiemri";
-int age = 20;
-Console.WriteLine($"Emri:{fullName} Mosha: {age}"); //string interpolation
+//Console.WriteLine("------ Variable Start -------");
+//string fullName = "Emri Mbiemri";
+//int age = 20;
+//Console.WriteLine($"Emri:{fullName} Mosha: {age}"); //string interpolation
 
-char gender = 'M';
-DateTime dob = DateTime.Now.AddDays(-20);
+//char gender = 'M';
+//DateTime dob = DateTime.Now.AddDays(-20);
 
-var firstName = "Emri";
-var lastName = 10;
-Console.WriteLine("------ Variable End -------");
+//var firstName = "Emri";
+//var lastName = 10;
+//Console.WriteLine("------ Variable End -------");
 
-Console.WriteLine("------ Class Start -------");
+//Console.WriteLine("------ Class Start -------");
 
-var newBook = new Book();
-newBook.Id = 1;
-newBook.Title = "Book Title";
-Console.WriteLine($"Book Id: {newBook.Id} Title: {newBook.Title}");
+//var newBook = new Book();
+//newBook.Id = 1;
+//newBook.Title = "Book Title";
+//Console.WriteLine($"Book Id: {newBook.Id} Title: {newBook.Title}");
 
-var secondBook = new Book(2, "Second Book");
-Console.WriteLine($"Book Id: {secondBook.Id} Title: {secondBook.Title}");
+//var secondBook = new Book(2, "Second Book");
+//Console.WriteLine($"Book Id: {secondBook.Id} Title: {secondBook.Title}");
+
+using BookStore.Tests;
+
+var emriStudenti1 = "Studenti 01";
+var emriStudenti2 = "Studenti 02";
+var emriStudenti3 = "Studenti 03";
+var emriStudenti4 = "Studenti 04";
+var emriStudenti5 = "Studenti 05";
+
+Console.WriteLine("-------> CONSOLE <-------");
+Console.WriteLine(emriStudenti1);
+Console.WriteLine(emriStudenti2);
+Console.WriteLine(emriStudenti3);
+Console.WriteLine(emriStudenti4);
+Console.WriteLine(emriStudenti5);
+
+
+var emriStudenteve = new List<string>
+{
+    "Studenti 01",
+    "Studenti 02",
+    "Studenti 03",
+    "Studenti 04",
+    "Studenti 05"
+};
+
+Console.WriteLine("-------> FOR <-------");
+
+for (int i = 0; i < emriStudenteve.Count; i++)
+{
+    Console.WriteLine(emriStudenteve[i]);
+}
+
+Console.WriteLine("-------> FOREACH <-------");
+
+foreach (var emriStudentit in emriStudenteve)
+{
+    Console.WriteLine(emriStudentit);
+}
+
+
+//Krijo nje liste per librat
+var libriIPreferuar = new Book(1, "Hyrje ne Statistike", 123);
+
+var libratEPreferuar = new List<Book> 
+{
+    new Book(1, "Hyrje ne Statistike", 123),
+    new Book(2, "Hyrje ne Matemetike", 345),
+    new Book(3, "Hyrje", 678)
+};
+
+Console.WriteLine("-------> FOREACH (LIBRAT) <-------");
+foreach (var libri in libratEPreferuar)
+{
+    Console.WriteLine($"Book Id: {libri.Id}, Title: {libri.Title}, Nr Faqeve: {libri.NrOfPages}");
+}
 
 string stopHere = "";
