14ddd5cc74948983f301fbb30f93945a2e40dfd4
“Ervis <ervistrupja@outlook.com>
2024-04-18 11:16:40 +0200

    krijimi i projektit mvc

---
+++ BookStore.App/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.App.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Modify()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore.App/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using BookStore.App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookStore.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
